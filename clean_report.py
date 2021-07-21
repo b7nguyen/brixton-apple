f578b4ec7b9bdcd6ca2c226f21b786eda420d10e
Binh Nguyen <b7nguyen@gmail.com>
2021-07-20 22:34:00 -0700

    added category

--- clean_report.py
+++ clean_report.py
@@ -35,7 +35,7 @@ import math
 
 PATH = "./input/orginial_reports"
 FILETRAIN = "/RetailSales2018-2021.xlsx"
-#FILECUSTOMER = "/customer_info_qb.xlsx"
+FILE_ANALYSIS = "/BC Mngd Retail Sales Analysis Q1 2021-2020_2021.05.03.xlsx"
 
 
 def readCSVFile(filename):
@@ -44,14 +44,27 @@ def readCSVFile(filename):
     return (pd.read_csv(file, chunksize=1000000))
 #%%
 
-def readXLSFile(filename):
+def readXLSFile(**kwargs):
     
-    file = PATH + filename
-    #file = filename
-    return (pd.read_excel(file))
+    if 'filename' in kwargs:
+        file = PATH + kwargs['filename']
+    else:
+        print("File name not provided")
+        return False
+    
+    if 'sheet' in kwargs:
+        xls = pd.ExcelFile(file)
+        ret_frame = pd.read_excel(xls, sheet_name=kwargs['sheet'])
+        #ret_frame = df[kwargs['sheets']]
+        
+        #ret_frame = ret_frame[kwargs['sheets']]
+    else:
+        ret_frame = pd.read_excel(file)
+    
+    return ret_frame
 
-def clean(data):
-    #drop first, second, and last row
+def clean_sales_report(data):
+    #drop 0, 2, and last row
     data.drop([0,2,len(data)-1], inplace=True)
     data.reset_index(drop=True, inplace=True)
     
@@ -84,17 +97,55 @@ def clean(data):
     data.reset_index(drop=True, inplace=True)
     
     #create column for lease ID
-    data['Lease ID'] = data['Lease Name']
-    data['Lease ID'] = data['Lease ID'].map(lambda x: 
-                                            x[x.find('(')+1:x.find(')')-1])
+#    data['Lease ID'] = data['Lease Name']
+#    data['Lease ID'] = data['Lease ID'].map(lambda x: 
+#                                            x[x.find('(')+1:x.find(')')-1])
     
-    
-    
-#%%
-    
+  
     return data
-()
+
+
+def clean_category_report(data):
+    #drop row index 0,1,2
+    data.drop([0,1,2], inplace=True)
+    data.reset_index(drop=True, inplace=True)
+    
+    #rename the columns with the first row and drop the row
+    data.columns = data.iloc[0]
+    data.drop([0], inplace=True)
+    data.reset_index(drop=True, inplace=True)
+    
+    #remove the white space from front and end of string
+    #TODO 
+    for i in data.columns:
+        data = data.rename(columns={i:i.lstrip().rstrip()}) 
+        
+        
+    data = data[data['Tenant Name'].isna()==False]
+    #data.drop([*data[data['Tenant Name'].isna()].index.values], inplace=True)
+  
+    #create column for lease ID
+#    data['Lease ID'] = data['Tenant Name']
+#    data['Lease ID'] = data['Lease ID'].map(lambda x: 
+#                                            x[x.find('(')+1:x.find(')')-1])
+
+    return data
+
+
+def joinCol(data1, data2):
+    data1 = data1.set_index('Lease Name')
+    data2 = data2.set_index('Tenant Name')
+    
+    data = pd.concat([data1, data2], join='inner', axis=1)
+
+    return data
+
 if __name__ == '__main__':
     
-    df_train = readXLSFile(FILETRAIN)
-    df_train = clean(df_train)
+    df_sales = readXLSFile(filename=FILETRAIN)
+    df_cat = readXLSFile(filename=FILE_ANALYSIS, sheet='Sales Category ')
+    
+    df_sales = clean_sales_report(df_sales)
+    df_cat = clean_category_report(df_cat)
+    
+    #df_sales = joinCol(df_sales, df_cat)
